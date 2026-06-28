2d8afd6939443376457ad2966644232c26ded928
Klein <klein.gachuhi@student.moringaschool.com>
2026-06-28 18:11:10 +0300

    Scroll eleganceeeeeeeeee

--- js/script.js
+++ js/script.js
@@ -142,4 +142,17 @@
             });
         }
     });
-})();
+})(); 
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        const href = this.getAttribute('href');
+        if (href === '#') return;
+        const target = document.querySelector(href);
+        if (target) {
+            e.preventDefault();
+            target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
